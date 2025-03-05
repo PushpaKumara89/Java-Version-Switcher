8d816e6184de6e8fa31f59429e238d0526b13d3e
PushpaKumara89 <kpushpakumarajd@gmail.com>
2025-03-05 23:12:56 +0530

    add list activate version error fix

--- mjv.bat
+++ mjv.bat
@@ -19,14 +19,20 @@ if not exist "%folder%" (
 )
 echo Available Java versions:
 for /d %%D in ("%folder%\*") do (
-    if exist "%%D\bin\java.exe" echo   %%~nxD
+    echo %JAVA_HOME% | findstr /C:"%%~nxD" >nul
+    if !errorlevel! equ 0 (
+        echo  * %%~nxD  - Currently activated Java version
+    ) else (
+        echo    %%~nxD
+    )   
+    
 )
 goto :end
 
 :use
 :: Check if the second argument is provided
 if "%~2"=="" (
-    echo Error: Please specify a Java version. Use `mjv list` to see available versions.
+    echo Error: Please specify a Java version. Use `mybat list` to see available versions.
     goto :end
 )
 set "java_path=C:\Program Files\Java\%~2"
@@ -35,48 +41,39 @@ if not exist "%java_path%\bin\java.exe" (
     goto :end
 )
 
-:: Remove old Java paths from user-level PATH
+:: Remove old Java paths
 for /f "tokens=2,*" %%A in ('reg query "HKCU\Environment" /v Path 2^>nul') do set "CURRENT_PATH=%%B"
 set "NEW_PATH="
 for %%P in (%CURRENT_PATH:;=%) do (
     echo %%P | findstr /i /c:"C:\Program Files\Java" >nul || set "NEW_PATH=!NEW_PATH!;%%P"
 )
+setx PATH "%java_path%\bin!NEW_PATH!" >nul
 
-:: Set user-level PATH and JAVA_HOME
-setx PATH "%NEW_PATH%;%java_path%\bin"
-if %errorlevel% neq 0 (
-    echo Error: Failed to set PATH.
-    goto :end
-)
-setx JAVA_HOME "%java_path%"
-if %errorlevel% neq 0 (
-    echo Error: Failed to set JAVA_HOME.
-    goto :end
-)
+:: Set JAVA_HOME
+setx JAVA_HOME "%java_path%" >nul
 
 echo Now using Java version: %~2
 echo Note: Restart your terminal for changes to take effect.
 goto :end
-
 
 :used
 java -version
 goto :end
 
 :help
-echo Usage: mjv [command] [arguments]
+echo Usage: mybat [command] [arguments]
 echo.
 echo Commands:
 echo   list         - Display available Java versions in C:\Program Files\Java
 echo   use [name]   - Set JAVA_HOME and update PATH for the specified Java version
-echo                  Example: mjv use jdk-17
+echo                  Example: mybat use jdk-17
 echo   used         - Show the currently active Java version
 echo   --help       - Show this help message
 echo.
 goto :end
 
 :unknown
-echo Error: Unknown command. Use `mjv --help` for usage information.
+echo Error: Unknown command. Use `mybat --help` for usage information.
 
 :end
 exit /b
